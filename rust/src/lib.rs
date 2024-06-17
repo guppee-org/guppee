cd4bd6f7a57d78fbd97e0ec96a86e6508c7b442e
Chris <89366859+chrisp60@users.noreply.github.com>
2024-06-16 22:10:11 -0400

    feat(rust): simple http server

---
+++ rust/src/lib.rs
@@ -0,0 +1,27 @@
+use axum::routing::Router;
+use tokio::net::TcpListener;
+use tower_http::services::ServeDir;
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error(transparent)]
+    Runtime(#[from] tokio::io::Error),
+}
+
+pub type Result<T, E = Error> = std::result::Result<T, E>;
+
+#[derive(Clone, Debug)]
+pub struct App {}
+
+impl App {
+    pub async fn start(dir: String) -> Result<()> {
+        let router = Router::new()
+            .route_service("/", ServeDir::new(dir.clone()))
+            .with_state(App {});
+        let addr = "127.0.0.1:8080";
+        let tcp_listener = TcpListener::bind(addr).await?;
+        println!("serving {dir} on {addr}");
+        axum::serve(tcp_listener, router).await?;
+        Ok(())
+    }
+}
---
+++ rust/src/main.rs
@@ -0,0 +1,31 @@
+use clap::{Parser, Subcommand};
+use tokio::runtime::Runtime;
+
+#[derive(Parser)]
+struct Args {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand, Debug)]
+enum Command {
+    /// Start the server.
+    Serve {
+        /// The directory to serve.
+        #[arg(short, long, default_value_t = String::from("dist"))]
+        dir: String,
+        /// The port to serve on.
+        #[arg(short, long, default_value_t = String::from("8080"))]
+        port: String,
+    },
+}
+
+fn main() -> server::Result<()> {
+    match Args::parse().command {
+        Command::Serve { dir, .. } => {
+            let rt = Runtime::new()?;
+            rt.block_on(server::App::start(dir))?;
+        }
+    }
+    Ok(())
+}
