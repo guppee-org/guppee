f4c70da7db84afde3b0ec9c476eef50d53cd1982
Chris <89366859+chrisp60@users.noreply.github.com>
2024-06-18 22:35:17 -0400

    ref(server): organization, span messages, docs

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,10 +1,21 @@
+use std::future::IntoFuture;
+
 use axum::routing::Router;
 use thiserror::Error;
-use tokio::net::TcpListener;
+use tokio::{net::TcpListener, select};
 use tower_http::{services::ServeDir, trace::TraceLayer};
+use tracing::{info, instrument, trace, Level};
 
-const DIST: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/dist");
+/// Result alias for [`Error`].
+pub type Result<T, E = Error> = std::result::Result<T, E>;
 
+/// The directory being served by the server.
+pub const DIST: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/../dist");
+
+/// The address being binded to by the server.
+pub const ADDR: &str = "0.0.0.0:8081";
+
+/// [`crate`] error types.
 #[derive(Error, Debug)]
 pub enum Error {
     #[error(transparent)]
@@ -13,17 +24,29 @@ pub enum Error {
     Parse(#[from] tracing_subscriber::filter::ParseError),
 }
 
-pub type Result<T, E = Error> = std::result::Result<T, E>;
-
 /// Start the http serve, serving the [`DIST`] directory.
-#[tracing::instrument(ret)]
+#[instrument(level = Level::TRACE, ret)]
 pub async fn start() -> Result<()> {
     let router = Router::new()
         .nest_service("/", ServeDir::new(DIST))
         .layer(TraceLayer::new_for_http());
 
-    let tcp_listener = TcpListener::bind("0.0.0.0:8081").await?;
-    tracing::debug!(tcp.addr = ?tcp_listener.local_addr());
-    axum::serve(tcp_listener, router).await?;
+    let tcp_listener = TcpListener::bind(ADDR).await?;
+
+    info!(
+        serve.address = ADDR,
+        serve.directory = DIST,
+        serve.url = format!("https://0.0.0.0:8081"),
+    );
+
+    let server_future = axum::serve(tcp_listener, router).into_future();
+    select! {
+        _ = tokio::signal::ctrl_c() => trace!(
+            "received ctrl-c signal. shutting down."
+        ),
+        _ = server_future => trace!(
+            "server future resolved, indicating a crash."
+        ),
+    }
     Ok(())
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,17 +1,27 @@
 use tokio::runtime::Runtime;
 use tracing_subscriber::EnvFilter;
 
+// Set logging level through RUST_LOG env variable.
+//
+// (Example): RUST_LOG=server=debug cargo run --release
+// https://docs.rs/tracing-subscriber/latest/tracing_subscriber/filter/struct.EnvFilter.html#directives
+
 fn main() -> server::Result<()> {
     let args = &mut std::env::args();
     let quiet = args.nth(1).is_some_and(|e| e == "--quiet");
 
     if !quiet {
         tracing_subscriber::fmt()
-            .with_env_filter(EnvFilter::try_from_default_env().unwrap_or("server=info".parse()?))
+            .with_env_filter(EnvFilter::try_from_default_env().unwrap_or("server=trace".parse()?))
             .pretty()
             .init();
         tracing::info!("tracing enabled")
     }
+
+    assert!(
+        std::path::Path::new(server::DIST).exists(),
+        "/dist directory must be located at the root of the guppee directory"
+    );
 
     let rt = Runtime::new()?;
     rt.block_on(server::start())?;
