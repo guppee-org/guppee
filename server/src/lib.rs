560b8de09126b393e8915638d536ab485df773dd
Chris <89366859+chrisp60@users.noreply.github.com>
2024-06-22 20:17:41 -0400

    feat(server): `Player` actor

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,15 +1,17 @@
-mod ws;
+mod actor;
+mod error;
 
 use std::future::IntoFuture;
 
-use axum::routing::{get, Router};
-use thiserror::Error;
+use axum::{routing::get, Json, Router};
 use tokio::{net::TcpListener, select};
 use tower_http::{services::ServeDir, trace::TraceLayer};
 use tracing::{info, instrument, trace, Level};
 
+use crate::actor::{Actor, Handle, PlayerId};
+
 /// Result alias for [`Error`].
-pub type Result<T, E = Error> = std::result::Result<T, E>;
+pub type Result<T, E = error::Error> = std::result::Result<T, E>;
 
 /// The directory being served by the server.
 pub const DIST: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/../dist");
@@ -17,21 +19,23 @@ pub const DIST: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/../dist");
 /// The address being binded to by the server.
 pub const ADDR: &str = "0.0.0.0:8081";
 
-/// [`crate`] error types.
-#[derive(Error, Debug)]
-pub enum Error {
-    #[error(transparent)]
-    Runtime(#[from] tokio::io::Error),
-    #[error(transparent)]
-    Parse(#[from] tracing_subscriber::filter::ParseError),
+/// Shared server state.
+#[derive(Clone, Debug)]
+struct App {
+    /// Communication channel with [`Actor`].
+    handle: crate::actor::Handle,
 }
 
 /// Start the http serve, serving the [`DIST`] directory.
 #[instrument(level = Level::TRACE, ret)]
 pub async fn start() -> Result<()> {
+    let handle = Actor::start();
+
     let router = Router::new()
-        .route("/ws", get(crate::ws::handler))
+        .route("/ws", get(websocket_handler))
+        .route("/players", get(player_list))
         .nest_service("/", ServeDir::new(DIST))
+        .with_state(App { handle })
         .layer(TraceLayer::new_for_http());
 
     let tcp_listener = TcpListener::bind(ADDR).await?;
@@ -52,4 +56,20 @@ pub async fn start() -> Result<()> {
         ),
     }
     Ok(())
+}
+
+#[instrument(skip_all)]
+async fn player_list(handle: Handle) -> Result<Json<Vec<PlayerId>>> {
+    let list = handle.players().await?;
+    Ok(Json(list))
+}
+
+#[instrument(skip_all)]
+pub async fn websocket_handler(
+    handle: Handle,
+    ws: axum::extract::WebSocketUpgrade,
+) -> axum::response::Response {
+    ws.on_upgrade(move |ws| async move {
+        handle.register(ws).await;
+    })
 }
