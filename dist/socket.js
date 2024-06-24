54a45d4c227976ad5cb227cb5d68c8685b06c652
Chris <89366859+chrisp60@users.noreply.github.com>
2024-06-23 20:00:18 -0400

    feat(client): populate player list
    The best javascript you have ever seen

--- dist/socket.js
+++ dist/socket.js
@@ -1,20 +1,37 @@
 function start_socket() {
   const url = "ws://" + location.host + "/ws";
   const ws = new WebSocket(url);
+  var whoami = null;
+
   ws.onmessage = (ev) => {
-    const msg = ev.data;
-    const parsed = JSON.parse(msg);
-    console.log(parsed);
+    if (whoami == null) {
+      whoami = JSON.parse(ev.data);
+      document.querySelector("#playerId").innerHTML = whoami.uuid;
+      return;
+    }
+    player_list(ev.data);
   };
 }
 
-async function player_list() {
-  const playlist = await fetch("/players");
-  console.log(playlist);
+function player_list(data) {
+  const players = JSON.parse(data);
+  var tbody = document.querySelector("#playerListBody");
+  tbody.innerHTML = ``;
+  for (const player of players) {
+    const content = `
+      <tr>
+        <td> ${player.uuid} </td>
+        <td> 
+          <button class="btn btn-primary btn-sm">Invite</button>
+        </td>
+      </tr>
+    `;
+    tbody.insertAdjacentHTML("beforeend", content);
+  }
 }
 
 function start_unity() {
-    const template = document.querySelector("#unity-container-wrapper");
-    const clone = template.content.cloneNode(true);
-    document.body.appendChild(clone);
+  const template = document.querySelector("#unity-container-wrapper");
+  const clone = template.content.cloneNode(true);
+  document.body.appendChild(clone);
 }
