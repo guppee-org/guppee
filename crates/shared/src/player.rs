820f5e1c49154f85a583b0faa14713b3143e8141
Chris <89366859+chrisp60@users.noreply.github.com>
2024-06-30 22:11:11 -0400

    feat(shared): `Player` and `PlayerId` impls

--- crates/shared/src/player.rs
+++ crates/shared/src/player.rs
@@ -1,9 +1,15 @@
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct PlayerId {
     repr: Uuid,
+}
+
+impl std::fmt::Display for PlayerId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.repr)
+    }
 }
 
 impl Default for PlayerId {
@@ -14,7 +20,26 @@ impl Default for PlayerId {
     }
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Player {
     id: PlayerId,
+    #[serde(default)]
+    display_name: Option<String>,
+}
+
+impl Player {
+    pub fn new() -> Self {
+        Self {
+            id: Default::default(),
+            display_name: Default::default(),
+        }
+    }
+
+    pub fn id(&self) -> PlayerId {
+        self.id
+    }
+
+    pub fn display_name(&self) -> Option<&str> {
+        self.display_name.as_deref()
+    }
 }
